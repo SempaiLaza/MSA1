fc832c3c9ce205b38486284bae017d913ec258df
lazzaka <saha.lazarenko.20033@gmail.com>
2024-12-15 04:06:31 +0300

    Add files via upload

---
+++ route-service/RouteInputDto.java
@@ -0,0 +1,14 @@
+package bsuir.laza.routeservice.entities;
+
+import lombok.Data;
+
+@Data
+public class RouteInputDto {
+    private Double startPointX;
+    private Double startPointY;
+
+    private Double endPointX;
+    private Double endPointY;
+
+    private Double weight;
+}
---
+++ route-service/RouteService.java
@@ -0,0 +1,11 @@
+package bsuir.laza.routeservice.service;
+
+import bsuir.laza.routeservice.entities.RouteInputDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RouteService {
+    public void calculateRoute(RouteInputDto input) {
+
+    }
+}
